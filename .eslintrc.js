1c22f8a7ec8b9cf5c8d40cf3f3ceb4fb9ca5c8a2
OjerIsaac <isaacojerumu@gmail.com>
2024-09-13 13:14:12 +0100

    chore: project setup 🗻

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,8 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'airbnb-base',
+    'airbnb-typescript/base',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
@@ -21,5 +23,14 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/prefer-default-export': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', args: 'all' }],
+    'consistent-return': 'off',
+    'class-methods-use-this': 'off',
+    'import/no-import-module-exports': 'off',
+    'import/no-cycle': 'off',
+    'no-unreachable-loop': 'off',
+    'no-await-in-loop': 'off',  
   },
 };
